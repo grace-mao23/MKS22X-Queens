361b2aadc2df53d803e0a7a63aa682403b8236a6
grace-mao23 <gmao00@stuy.edu>
2019-02-07 19:51:13 -0500

    (Re)filled in constructor and wrote private addQueen method

--- QueenBoard.java
+++ QueenBoard.java
@@ -2,8 +2,24 @@ public class QueenBoard {
   private int[][] board;
 
   public QueenBoard(int size){
-
+    board = new int[size][size];
   }
+
+  private boolean addQueen(int r, int c) {
+    if (board[r][c] != 0) {
+      return false;
+    }
+    board[r][c] = -1;
+    for (int i = 0; i < board.length; i++) {
+      for (int x = c + 1; x < board.length; x++) {
+        if (i == r || Math.abs(r-i) == Math.abs(c-x)) {
+          board[i][x] += 1;
+        }
+      }
+    }
+    return true;
+  }
+
 
   /**
   *@return The output string formatted as follows:
